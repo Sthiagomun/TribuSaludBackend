47fddc6d4be1b596e9d544345d539db0418c0bbd
Santiago Munoz Colorado <sthiagomun@gmail.com>
2025-05-07 14:46:15 -0500

    Agregando el frontend para los ensallos

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsId('btn-login').addEventListener('click', function() {
+    window.location.href = '../html/login_index.html';
+});
+document.getElementsById('btn-register').addEventListener('click', function() {
+    window.location.href = '../html/register_index.html';
+});
