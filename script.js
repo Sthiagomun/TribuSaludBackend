6f9b55c40188fc5d53e1fc1b2fef369d7dbfbaf2
Santiago Munoz Colorado <sthiagomun@gmail.com>
2025-05-07 16:42:37 -0500

    Agregando librerias y acomodando las carpetas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('btn-login').addEventListener('click', function() {
-    window.location.href = '../html/login_index.html';
+    window.location.href = '../web/html/login_index.html';
 });
 document.getElementById('btn-register').addEventListener('click', function() {
-    window.location.href = '../html/register_index.html';
+    window.location.href = '../web/html/register_index.html';
 });
