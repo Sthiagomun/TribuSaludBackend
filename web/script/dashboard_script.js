9a73ed4316fe830ac607f5c7f1d2417dce7b538c
Santiago Munoz Colorado <sthiagomun@gmail.com>
2025-05-07 17:01:58 -0500

    Se aha agregado el dashboard y acomodado redirecciones

---
+++ web/script/dashboard_script.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-solicitar-cita').addEventListener('click', function() {
+    window.location.href = '/web/html/solicitar_index.html';
+});
+document.getElementById('btn-citas').addEventListener('click', function() {
+    window.location.href = '/web/html/citas_index.html';
+});
+document.getElementById('btn-cancelar-cita').addEventListener('click', function() {
+    window.location.href = '/web/html/cancelar_index.html';
+});
